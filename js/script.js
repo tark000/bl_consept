313afead3eda4d0d757774d5ef2d557c695f6c0e
taras <tark000@ukr.net>
2016-01-08 01:46:14 +0200

    add tab

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 
 $(function() {
-    console.log("ready");
+	var options = { to: { width: 500, height: 200 } };
+	$('#tabs-1').tabs({
+	    hide: { effect: "scale",options, duration: 400 },
+	    show: { effect: "scale",options, duration: 400 }
+	});
 });
 
